64e92e87c70e5b41eef26e7157e0d4e1ca761b91
Hmida-Rojbani <hmida.rojbani@fst.utm.tn>
2023-02-13 16:11:08 +0100

    get

--- s5/routers/students.js
+++ s5/routers/students.js
@@ -7,4 +7,15 @@ router.post('/',async (req, res) =>{
     res.status(201).send(student);
 })
 
+router.get('/',async (req, res) =>{
+    let students = await Student.find();
+    res.status(200).send(students);
+})
+
+router.get('/id/:id',async (req, res) =>{
+    let student = await Student.findById(req.params.id);
+    if(!student)
+        return res.status(404).send('Student is not found')
+    res.status(200).send(student);
+})
 module.exports=router;

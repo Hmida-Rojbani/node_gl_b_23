ebe22426396c3e8ca54582e9b9bf25ff5eacc000
Hmida-Rojbani <hmida.rojbani@fst.utm.tn>
2023-02-27 13:49:26 +0100

    recherche specific

--- s5/routers/students.js
+++ s5/routers/students.js
@@ -21,4 +21,17 @@ router.get('/id/:id',async (req, res) =>{
         return res.status(404).send('Student is not found')
     res.status(200).send(student);
 })
+// search via active = true
+router.get('/active',async (req, res) =>{
+    let students = await Student.find({active:'true'});
+    res.status(200).send(students);
+})
+// search via active = false
+router.get('/not/active/size/:size/page/:page',async (req, res) =>{
+    let students = await Student.find({active:'false'})
+                                .skip((req.params.page-1)*req.params.size)
+                                .limit(req.params.size);
+                                
+    res.status(200).send(students);
+})
 module.exports=router;
